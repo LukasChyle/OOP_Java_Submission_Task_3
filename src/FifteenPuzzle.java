d974db3f16b58619ee179ff03569efe8420b80f7
Lukas Chyle <lukas_chyle@hotmail.com>
2022-10-28 16:14:56 +0200

    Initial commit

---
+++ src/FifteenPuzzle.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FifteenPuzzle extends JFrame implements ActionListener {
+
+    private FifteenPuzzle() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        FifteenPuzzle game = new FifteenPuzzle();
+    }
+}
