760b10caa4ec5d9c128e6f0453f456e05f2bcc31
Lukas Chyle <lukas_chyle@hotmail.com>
2022-10-29 14:52:27 +0200

    Merge Conflict

--- src/FifteenPuzzle.java
+++ src/FifteenPuzzle.java
@@ -8,7 +8,6 @@ import java.util.Random;
 
 public class FifteenPuzzle extends JFrame implements ActionListener {
     private final JButton[][] board = new JButton[4][4];
-
     private final JButton restart = new JButton("Restart");
     private final List<Integer> checkUnique = new ArrayList<>();
     private final String[][] winOrder = {
@@ -38,7 +37,6 @@ public class FifteenPuzzle extends JFrame implements ActionListener {
         }
     }
 
-
     private int getRandom() {
         while (true) {
             Random random = new Random();
@@ -51,7 +49,7 @@ public class FifteenPuzzle extends JFrame implements ActionListener {
         }
     }
 
-    private boolean checkNumber(int number){
+    private boolean checkNumber(int number) {
         for (int i : checkUnique) {
             if (i == number) {
                 return false;
@@ -64,11 +62,9 @@ public class FifteenPuzzle extends JFrame implements ActionListener {
         for (int i = 0; i < (board.length); i++) {
             for (int j = 0; j < board.length; j++) {
                 board[i][j] = new JButton(String.valueOf(getRandom()));
+                board[i][j].addActionListener(this);
                 if (board[i][j].getText().equals("0")) {
                     board[i][j].setBackground(new Color(255, 255, 255));
-                    board[i][j].addActionListener(this);
-                if (board[i][j].getText().equals("0")) {
-                    board[i][j].setBackground(Color.white);
                     board[i][j].setText("");
                     board[i][j].setBorderPainted(false);
                 }
@@ -76,8 +72,7 @@ public class FifteenPuzzle extends JFrame implements ActionListener {
         }
     }
 
-
-    private boolean checkOrderWin(List<Integer> checkUnique, String[][] winOrder) {
+    private boolean checkOrderWin() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (!board[i][j].getText().equals(winOrder[i][j])) {
@@ -89,31 +84,11 @@ public class FifteenPuzzle extends JFrame implements ActionListener {
     }
 
 /*
-checkOrder:
-kontrollera om siffrorna ligger i rätt ordning på knapparna "getText()"
-genom att iterera genom knapparna och jämföra mot motsvarande plats på winOrder
-om man vunnit gör en pop up ruta med vinst och kalla sen på newGame
-/*
-
-/*
 newGame:
 board = new JButton[4][4];
 checkUnique = new int[16];
 kalla på setButtons
 ta bort markering från knapp (kanske inte behövs)
-*/
-
-/*
-Listener even:
-Markera knappen om den är bredvid "0"
-när man klickar på nästa knapp:
-- om knappen inte är "0" men också ligger bredvid "0", flytta över markeringen till nya knappen.
-- om knappen inte är "0" och inte ligger bredvid "0" släck markeringen på nuvarande.
-- om knappen ligger bredvid "0" byt plats på talen(texterna) och släck markering.
-
-Kör checkOrder för att se om pusslet är klart.
-------------------
-om restart knappen trycks kalla på newGame
 */
 
     private int[] getPosition(String text) {
@@ -127,7 +102,7 @@ om restart knappen trycks kalla på newGame
         return null;
     }
 
-    private void resetColors(){
+    private void resetColors() {
         for (JButton[] jButtons : board) {
             for (int j = 0; j < board.length; j++) {
                 if (!jButtons[j].getText().equals("")) {
@@ -144,14 +119,12 @@ om restart knappen trycks kalla på newGame
         int[] clicked = getPosition(e.getActionCommand());
 
         if ((blank[0] == clicked[0] && (blank[1] == (clicked[1] - 1) || blank[1] == (clicked[1] + 1))) ||
-        (blank[1] == clicked[1] && (blank[0] == (clicked[0] - 1) || blank[0] == (clicked[0] + 1)))) {
+                (blank[1] == clicked[1] && (blank[0] == (clicked[0] - 1) || blank[0] == (clicked[0] + 1)))) {
             board[clicked[0]][clicked[1]].setBackground(clickColor);
         }
 
 
-
-
-        if(checkOrderWin(checkUnique, winOrder)){
+        if (checkOrderWin()) {
             JOptionPane.showMessageDialog(null, "You solved it!");
         }
     }
